c96c8786019b8d272b34c60473e0bd538c34b588
FeridQuluzade <feridquluzade2002@gmail.com>
2022-03-21 16:19:14 +0400

    initial commit

---
+++ src/main/java/az/unitech/deveopment/auth/MsAuthApplication.java
@@ -0,0 +1,13 @@
+package az.unitech.deveopment.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthApplication.class, args);
+    }
+
+}
