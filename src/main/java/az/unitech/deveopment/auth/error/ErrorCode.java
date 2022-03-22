402f52748c2923a2f4e057a90711bdd056bae0e0
FeridQuluzade <feridquluzade2002@gmail.com>
2022-03-23 00:42:29 +0400

    add pin constraint for userCreateDto migrate database atlassian

---
+++ src/main/java/az/unitech/deveopment/auth/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package az.unitech.deveopment.auth.error;
+
+public interface ErrorCode {
+
+    ErrorCode UNKNOWN_ERROR_CODE = UnknownErrorCode.INSTANCE;
+
+    String code();
+
+    enum UnknownErrorCode implements ErrorCode {
+        INSTANCE;
+
+        @Override
+        public String code() {
+            return "unknown";
+        }
+    }
+
+}
---
+++ src/main/java/az/unitech/deveopment/auth/error/validation/Patterns.java
@@ -0,0 +1,10 @@
+package az.unitech.deveopment.auth.error.validation;
+
+public final class Patterns {
+
+    public static final String DocumentPIN = "^[0-9,A-NP-Z]{5,7}$";
+
+    private Patterns() {
+    }
+
+}
